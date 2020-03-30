57f33055ad6ce5ce3186043d2d9b1dafed13d28e
Ben McManus <bennoislost@users.noreply.github.com>
2020-03-30 15:41:26 +0100

    Disable Guzzle exceptions

--- src/Inviqa/Clearpay/Http/Factory.php
+++ src/Inviqa/Clearpay/Http/Factory.php
@@ -14,11 +14,12 @@ class Factory
         return new Client(
             new GuzzleAdapter(
                 new Guzzle([
-                    'base_uri' => $config->uri(),
-                    'auth'     => [
+                    'base_uri'    => $config->uri(),
+                    'auth'        => [
                         $config->username(),
                         $config->password()
-                    ]
+                    ],
+                    'http_errors' => false
                 ])
             ),
             new GuzzleMessageFactory()
