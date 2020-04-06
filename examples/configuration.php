40968d4f278622651e2b693935f21515a6602246
Ben McManus <bennoislost@users.noreply.github.com>
2020-04-06 10:23:33 +0100

    Update configuration example

--- examples/configuration.php
+++ examples/configuration.php
@@ -5,7 +5,11 @@ require_once __DIR__ . '/_bootstrap.php';
 $app = new \Inviqa\Clearpay\Application(config());
 
 try {
-    var_dump($app->getConfiguration());
+    $result = $app->getConfiguration();
+
+    echo "Minimum Amount: " . $result->getMinimumAmount() . PHP_EOL;
+    echo "Maximum Amount: " . $result->getMaximumAmount() . PHP_EOL;
+    echo "Currency:       " . $result->getCurrencyCode() . PHP_EOL;
 }
 catch (Exception $e) {
     echo "Code: " . $e->getCode() . PHP_EOL;
